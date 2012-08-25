1f5c7dda82dd47fedbda5fe5626f0abd65908a3b
Sam Saccone <samccone@gmail.com>
2012-08-25 17:25:12 -0400

    initial cutout setup and structure

---
+++ js/cutout.js
@@ -0,0 +1,57 @@
+var frameInterval;
+var frameRate = 24;
+var holder    = document.createElement('canvas');
+var output    = document.createElement('canvas');
+var videoElm  = document.createElement('video');
+var setup     = false;
+var debug     = false;
+
+document.body.appendChild(output);
+videoElm.addEventListener('loadedmetadata', metaLoadeds);
+videoElm.src = "video.mp4";
+videoElm.load();
+
+function metaLoadeds() {
+  if (!setup) {
+    setupEnv();
+    setup = true;
+  }
+  videoElm.play();
+}
+
+// places image data onto the dummy canvas and then into the display one
+function placeOntoCanvas() {
+  holder.getContext('2d').drawImage(videoElm, 0, 0);
+  data = holder.getContext('2d').getImageData(0, 0, holder.width, holder.height);
+  cleanData(data, function(cleaned) {
+    output.getContext('2d').putImageData(cleaned, 0, 0);
+  });
+}
+
+// Takes care of the cutout
+function cleanData(area, cb) {
+  for(var i = 0, l = area.data.length; i < l; i += 4) {
+    if(area.data[i + 1] >= 170) {
+      area.data[i+3] = 0;
+    }
+  }
+  cb(area);
+}
+
+
+function setupEnv() {
+  holder.setAttribute('width', videoElm.videoWidth);
+  holder.setAttribute('height', videoElm.videoHeight);
+  output.setAttribute('width', videoElm.videoWidth);
+  output.setAttribute('height', videoElm.videoHeight);
+
+  videoElm.addEventListener('play', function() {
+    frameInterval = setInterval(placeOntoCanvas, 1000 / frameRate);
+  });
+
+  videoElm.addEventListener('pause', function() { debug && console.log("pause"); clearInterval(frameInterval); });
+  videoElm.addEventListener('ended', function() { debug && console.log("ended"); clearInterval(frameInterval); videoElm.load(); });
+  videoElm.addEventListener('error', function() { debug && console.log("error"); clearInterval(frameInterval); });
+  videoElm.addEventListener('suspend', function() { debug && console.log("suspend"); clearInterval(frameInterval); });
+  videoElm.addEventListener('emptied', function() { debug && console.log("emptied"); clearInterval(frameInterval); });
+}
